d3818f87d0ee3226e8367aeab28b9d5cc3274d6b
Veerengiri <virengirigoswami3@gmail.com>
2024-10-08 19:58:07 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Compney from "./pages/Compney/Compney";
 import Contact from "./pages/Contact/Contact";
-
-const App = () => {
-  return (
-    <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/info" element={<Compney />} />
-      <Route path="/contact" element={<Contact />} /> */}
-      <Route
-        path="/"
-        element={
-          <>
-            <Home />
-            <About />
-            <Compney />
-            <Contact />
-          </>
-        }
-      />
-    </Routes>
-  );
+import Splash from "./components/Loader/Splash";
+const AppWrapper = () => {
+  const [showSplash,setShowSplash]=useState(true);
+  useEffect(()=>{
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 5000);
+  },[])
+  return (<>
+    {showSplash && <Splash/>}
+    <Home />
+    <About />
+    <Compney />
+    <Contact />
+  </>);
 };
 
-const AppWrapper = () => (
-  <Router>
-    <App />
-  </Router>
-);
-
 export default AppWrapper;
+
+
+
