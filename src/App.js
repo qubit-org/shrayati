b0c9f827d78301ba4d491d784b5437f0013295e7
Veerengiri <virengirigoswami3@gmail.com>
2024-10-10 14:32:23 +0530

    Loader Working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Compney from "./pages/Compney/Compney";
 import Contact from "./pages/Contact/Contact";
 import Splash from "./components/Loader/Splash";
 import SideNav from "./components/Nav/SideNav";
+import MainSlider from "./components/MainSlider/MainSlider";
 const AppWrapper = () => {
   const [showSplash, setShowSplash] = useState(true);
-  useEffect(() => {
+  const [imgLoading, setImgLoading] = useState(true);
+  useEffect(()=>{
     setTimeout(() => {
       setShowSplash(false);
-    }, 6000);
-    const handleWindowLoad = () => {setShowSplash(false);};
-    window.addEventListener("load", handleWindowLoad);
-    return () => {
-      window.removeEventListener("load", handleWindowLoad);
-    };
-  },[]);
- 
+    }, 3000);
+  },[])
   return (
     <div id="App-Overlay">
-      {showSplash && <Splash />}
-      <Home />
+      {(showSplash || imgLoading) && <Splash />}  
+      <MainSlider setImgLoading={setImgLoading} />
       <SideNav />
       <About />
       <Compney />
